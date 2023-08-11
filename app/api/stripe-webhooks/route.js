fac17c982265b39f4d2d5925e4af2cccbfaa2c2b
Ashmit Sharma <sashmit2003@gmail.com>
2023-08-11 16:32:17 +0530

    check

--- app/api/stripe-webhooks/route.js
+++ app/api/stripe-webhooks/route.js
@@ -1,32 +1,13 @@
-// api/webhooks.js
+  const handler = async (req, res) => {
+    console.log("outit")
 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      try {
-        // Assuming you have set up your Stripe webhook secret and handling logic
-        // Make sure to replace 'YOUR_STRIPE_WEBHOOK_SECRET' with your actual webhook secret
-        const stripe = require('stripe')('STRIPE_SECRET_KEY');
-        const endpointSecret = 'STRIPE_WEBHOOK_SECRET';
-        console.log('Webhook kuch toh hua: true');
-
-        // Verify the signature of the incoming webhook payload
-        const sig = req.headers['stripe-signature'];
-        const event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-  
-        // Handle the specific event types you're interested in
-        if (event.type === 'checkout.session.completed') {
-          // Perform necessary actions when account is updated
-          console.log('Webhook connected: true');
-        }
-  
-        res.status(200).json({ received: true });
-      } catch (error) {
-        console.error('Error handling Stripe webhook:', error.message);
-        res.status(400).end();
-      }
+    if (req.method === "POST") {
+      // Code here
+      console.log("sinit")
     } else {
-      res.setHeader('Allow', 'POST');
-      res.status(405).end('Method Not Allowed');
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method Not Allowed");
     }
-  }
+  };
   
+  export default handler;
